7f2fd655e02631951baea53edd92e243883ff5ec
Kevin Louis <keviinlouiis@gmail.com>
2018-10-02 01:08:03 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('uploadTmp', 'UtilsController@uploadTmp')->name('upload.tmp');
+Route::delete('removeTmp/{arquivo}', 'UtilsController@removeTmp')->name('remove.tmp');
+Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
