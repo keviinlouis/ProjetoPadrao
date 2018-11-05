44d8fcfc151c0a848b0cf163cbabdb8a67de8ba1
Kevin Louis <keviinlouiis@gmail.com>
2018-11-04 23:47:12 -0200

    Update ScopeDistance.php

--- app/Traits/ScopeDistance.php
+++ app/Traits/ScopeDistance.php
@@ -16,20 +16,20 @@ trait ScopeDistance
     public $fieldDistance = 'distancia';
     public $tableRelated = 'enderecos';
 
-    public function scopeWhereDistance(Builder $query, float $latitude, float $longitude, float $raio, \Closure $where = null)
-    {
-        $latitude = str_replace(',', '.', $latitude);
-        $longitude = str_replace(',', '.', $longitude);
+    public function scopeWhereMorphDistance(
+        Builder $query,
+        float $latitude,
+        float $longitude,
+        float $raio,
+        \Closure $where = null
+    ) {
 
-        $sql = "((ACOS(SIN({$latitude} * PI() / 180) * SIN({$this->tableRelated}.latitude * PI() / 180) +
-                COS({$latitude} * PI() / 180) * COS({$this->tableRelated}.latitude * PI() / 180) *
-            COS(({$longitude} - {$this->tableRelated}.longitude) * PI() / 180)) * 180 / PI()) * 60 * 1.853159616)";
-
-        $query->selectRaw($this->getTable() . ".*, $sql as " . $this->fieldDistance);
+        $query->selectRaw($this->getTable() . ".*, " . $this->buildSqlString($latitude, $longitude, $this->tableRelated, self::getDistanceField()));
 
         $query->from($this->getTable());
 
-        $query->join($this->tableRelated, $this->tableRelated . '.entidade_id', '=', $this->getTable() . '.'. $this->getKeyName());
+        $query->join($this->tableRelated, $this->tableRelated . '.entidade_id', '=',
+            $this->getTable() . '.' . $this->getKeyName());
 
         $query->where($this->tableRelated . '.entidade_type', '=', get_class($this));
 
@@ -37,11 +37,55 @@ trait ScopeDistance
             $query->where($where);
         }
 
-        $query->whereRaw("$sql <= $raio");
+        $query->whereRaw($this->buildSqlString($latitude, $longitude, $this->tableRelated)." <= ".$this->clearStringLatLong($raio));
+
     }
 
     public static function getDistanceField()
     {
         return (new self())->fieldDistance;
+    }
+
+    public function scopeWhereDistance(
+        Builder $query,
+        float $latitude,
+        float $longitude,
+        float $raio,
+        \Closure $where = null)
+    {
+        $query->selectRaw($this->getTable() . ".*, " . $this->buildSqlString($latitude, $longitude, $this->getTable(), self::getDistanceField()));
+
+        $query->from($this->getTable());
+
+        if ($where) {
+            $query->where($where);
+        }
+
+        $query->whereRaw($this->buildSqlString($latitude, $longitude, $this->getTable())." <= ".$this->clearStringLatLong($raio));
+
+    }
+
+    private function clearStringLatLong($string)
+    {
+        return str_replace(',', '.', $string);
+    }
+
+    private function buildSqlString($latitude, $longitude, $table, string $as = '')
+    {
+
+        $latitude = $this->clearStringLatLong($latitude);
+        $longitude = $this->clearStringLatLong($longitude);
+
+        $calc = $this->clearStringLatLong((string) 1.853159616);
+
+        $sql = "((ACOS(SIN({$latitude} * PI() / 180) * SIN({$table}.latitude * PI() / 180) +
+                COS({$latitude} * PI() / 180) * COS({$table}.latitude * PI() / 180) *
+            COS(({$longitude} - {$table}.longitude) * PI() / 180)) * 180 / PI()) * 60 * {$calc})";
+
+        if(strlen($as)){
+            $sql .= " as {$as}";
+        }
+
+        return $sql;
     }
 }
