43b0cb0357e6e805487002c4748113c8b66b4c81
Kevin Louis <keviinlouiis@gmail.com>
2018-11-04 22:59:00 -0200

    Add ReflectionTrait on BaseModel

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 use App\Traits\AttributesMasks;
+use App\Traits\ReflectionMethodsTrait;
 use Illuminate\Database\Eloquent\Model;
 
 abstract class BaseModel extends Model
 {
-    use AttributesMasks;
+    use AttributesMasks, ReflectionMethodsTrait;
 }
